ad5eec7581c5bf23882597127a56b17a989fee92
vampire-nut <nuttapong.developer@gmail.com>
2025-06-08 20:43:48 +0700

    fix name NEXT_PUBLIC_BACKEND_URL

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const BACKEND_URL = process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:5000';
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000';
 const socket = io(BACKEND_URL);
 
 socket.on('connect', () => {
