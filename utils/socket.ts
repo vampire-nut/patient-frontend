82023e9ccb5bdcdcf434f00eff66b3ec4c63bb93
vampire-nut <nuttapong.developer@gmail.com>
2025-06-08 20:58:12 +0700

    fix disconnect socket

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,14 +1,23 @@
 import { io } from 'socket.io-client';
 
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000';
-const socket = io(BACKEND_URL);
+const socket = io(BACKEND_URL, {
+  reconnectionAttempts: 5, 
+  reconnectionDelay: 1000, 
+  reconnectionDelayMax: 5000, 
+  forceNew: true 
+});
 
 socket.on('connect', () => {
   console.log('Connected to WebSocket server');
 });
 
-socket.on('disconnect', () => {
-  console.log('Disconnected from WebSocket server');
+socket.on('disconnect', (reason) => {
+  console.log('Disconnected from WebSocket server. Reason:', reason);
+  if (reason === 'io server disconnect') {
+    console.log('Server initiated disconnect, attempting to reconnect...');
+    socket.connect();
+  }
 });
 
 socket.on('connect_error', (err) => {
